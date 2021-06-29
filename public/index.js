39710bcb688146f60fa32d7ac84beb67404636d3
Masho Ado <mashoado@gmail.com>
2021-06-29 22:57:49 +0300

    add event listerners to display menu in mobile,display the back this project section

---
+++ public/index.js
@@ -0,0 +1,24 @@
+const menuBtn = document.getElementById("menu-btn")
+const menu = document.getElementById("menu")
+const menuLogo = document.getElementById("menu-button")
+const backProjectBtn = document.getElementById("back-this-project-button")
+const backProject = document.getElementById("back-this-project")
+const closeBackProject = document.getElementById("close-button-back-project")
+const main = document.getElementById("main")
+const radioBtn = document.querySelectorAll("#radio-btn")
+const radio = document.querySelectorAll(".radio")
+
+// when clicked display menu
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("show-menu")
+  main.classList.toggle("opacity")
+});
+// When clicked it displays the back project section
+backProjectBtn.addEventListener("click", () => {
+  backProject.classList.add("show")
+})
+// Remove the back project section
+closeBackProject.addEventListener("click", () => {
+  backProject.classList.remove("show")
+})
+
